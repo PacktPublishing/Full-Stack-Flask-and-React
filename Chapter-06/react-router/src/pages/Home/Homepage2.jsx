a25de76d4b6e632142d8418618e28c7b5a4b0e8e
Olatunde Adedeji <olatundeadedeji32@gmail.com>
2022-08-11 15:20:48 +0100

    updated chapter-6

---
+++ Chapter-06/react-router/src/pages/Home/Homepage2.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage2 = () => {
+  return <div>
+
+    <h1>Route URL: localhost:3000/home/homepage2</h1>
+	<h2>You are inside the Homepage2 Component</h2>
+  </div>;
+};
+
+export default Homepage2;
