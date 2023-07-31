691a57bcf899f137964256cdae79b23375bc8d72
Olatunde Adedeji <olatundeadedeji32@gmail.com>
2023-07-31 08:27:21 +0100

    added webStacksData

---
+++ Chapter-05/data/webStacksData.js
@@ -0,0 +1,48 @@
+
+export const webStacksData = [
+
+
+		{
+			id: 1,
+			name: "Web Development Stacks",
+			values: []
+		  },
+      {
+
+        id: 2,
+        name: "Backend development",
+        values: [
+          {
+            id: 3,
+            name: "NodeJS"
+          },
+          {
+            id: 4,
+            name: "Flask",
+            values: []
+          }
+        ]
+      },
+      {
+        id: 5,
+        name: "Frontend development",
+        values: [
+			{
+				id: 6,
+				name: "React"
+			  },
+			  {
+				id: 7,
+				name: "VueJS",
+				values: []
+			  }
+		]
+      },
+	    {
+        id: 8,
+        name: "Containeriztion & Deployment",
+        values: []
+      }
+
+
+];
