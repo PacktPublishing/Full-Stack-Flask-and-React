1bcb5e0614f626050c8eb2ade8e77b99db18b348
Olatunde Adedeji <olatundeadedeji32@gmail.com>
2023-08-19 17:49:04 +0100

    Update Header.jsx

--- Chapter-05/06/frontend/src/components/Header/Header.jsx
+++ Chapter-05/06/frontend/src/components/Header/Header.jsx
@@ -9,16 +9,11 @@ const Header = () => {
         <nav className="nav">
           <ul>
 		  <li><Link to="/" className='navlink'>Home</Link></li>
-            <li><Link to="/about" className='navlink'>About</Link></li>
+          
             <li><Link to="/speakers" className='active navlink'>Speakers</Link></li>
-            <li><Link to="/events" className='navlink'>Events</Link></li>
-            <li><Link to="/sponsors" className='navlink'>Sponsors</Link></li>
-            <li><Link to="/contact" className='navlink'>Contact</Link></li>
+   
           </ul>
-        </nav>
-        <div className="auth">
-          <button className="btn">Login</button>
-        </div>
+        </nav>    
     </header>
   );
 }
