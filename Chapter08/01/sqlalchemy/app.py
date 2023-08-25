01ca9545a277aa11b947236b53efc9145e2518ce
Olatunde Adedeji <olatundeadedeji32@gmail.com>
2023-08-25 10:51:32 +0100

    Chapter08

---
+++ Chapter08/01/sqlalchemy/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__, template_folder='templates')
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///bizza.db"
+db = SQLAlchemy(app)
+
+
+@app.route('/users')
+def get_users():
+    users = User.query.all()
+    return render_template('users.html', users=users)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(55), unique=True)
+    name = db.Column(db.String(55), unique=False)
+    email = db.Column(db.String(100), unique=True)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.name)
---
+++ Chapter08/03/models/users.py
@@ -0,0 +1,15 @@
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
